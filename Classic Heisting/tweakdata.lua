3b2a1729e0a8fd43c0340e98a371868743df03c5
RedFlamer <76979559+RedFlamer@users.noreply.github.com>
2021-01-10 00:23:29 +0000

    Add files via upload

---
+++ Classic Heisting/tweakdata.lua
@@ -0,0 +1,21 @@
+local self = tweak_data
+self.difficulties = {
+	"easy",
+	"normal",
+	"hard",
+	"overkill",
+	"overkill_145",
+	"overkill_290",
+	"sm_wish",
+	"sm_wish"
+}
+self.difficulty_level_locks = {
+	0,
+	0,
+	0,
+	0,
+	0,
+	80,
+	69420,
+	69420
+}	
